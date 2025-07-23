befe4a672a7e2f2701b70a90757b953fcd6f16cf
Cody Spath <cody.s@boostpoint.com>
2025-07-23 09:27:01 -0400

    feat: use hash history to make github pages happy

--- apps/web/src/components/Headers/Header.vue
+++ apps/web/src/components/Headers/Header.vue
@@ -8,7 +8,7 @@ const props = defineProps<HeaderProps>();
 </script>
 
 <template>
-  <h1 :class="[twMerge('text-2xl font-black text-gray-800 dark:text-gray-300', props.class)]">
+  <h1 :class="[twMerge('text-2xl text-gray-700 dark:text-gray-300', props.class)]">
     <slot />
   </h1>
 </template>
